2106b4ee17fa56a91fc3341948edf88c12332995
Rubens <rubandrumline@gmail.com>
2024-01-15 23:58:44 -0300

    Sooo much stuff

--- build/index.js
+++ build/index.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log("Teste");
+Object.defineProperty(exports, "__esModule", { value: true });
+const CsvFileReader_1 = require("./CsvFileReader");
+const MatchResult_1 = require("./MatchResult");
+const reader = new CsvFileReader_1.CsvFileReader("football.csv");
+reader.read();
+let manUnitedWins = 0;
+for (let match of reader.data) {
+    if (match[1] === "Man United" && match[5] === MatchResult_1.MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === "Man United" && match[5] === MatchResult_1.MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
